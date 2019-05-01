e794e2d67a36184ab4821b875142d87decae4f1d
Yusuke Sano <toyoko.app@gmail.com>
2019-05-01 22:48:23 +0900

    タブとコンポーネントの追加
    ・結合中
    ・いままでの分を結合中

--- main.js
+++ main.js
@@ -10,19 +10,25 @@ window.addEventListener('load',function(){
     components:[
       'url:bootstrap/init.vue',
       'url:bootstrap/introduce.vue',
+      'url:component/form/data-table.vue',
+      'url:component/form/numeric-up-down.vue',
     ],
+    data:{
+      isReady:false
+    },
     methods:{
       startup:function(fingerprint,json){
-        console.log(fingerprint,json)
       },
       setup:function(name,data){
-        console.log(name,data)
+        this.isReady = true
       }
     },
     created:function(){
       this.parameter = new URLSearchParams(window.location.search)
     },
     mounted:function(){
+      $('.menu .item').
+        tab('change tab','task')
     }
   })
 
